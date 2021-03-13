a1219a472303f143883e5275763b2b9d69d2adf0
Drull <pcaetanop@gmail.com>
2021-03-13 16:00:33 -0300

    Class

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import Image from "../components/Image";
 import PostContainer from "../components/PostContainer";
+import styles from "../styles/Home.module.css"
 
 function Home() {
   return (
     <div>
       <Image />
+      <h1 className={styles.Text}>Destinations waiting for you:</h1>
       <PostContainer />
     </div>
   );
