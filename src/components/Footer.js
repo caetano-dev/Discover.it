4c0a0898dce5394d564c8ef1493e3b8e1d760371
Drull <pcaetanop@gmail.com>
2021-03-14 07:42:11 -0300

    Creating footer

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "../styles/components/Footer.module.css";
+import { FaGithub } from "react-icons/fa";
+function Footer() {
+  return (
+    <div>
+      <footer className={styles.Footer}>
+        <div>
+          <h1>Discover.it</h1>
+          <p>Made for studying purposes</p>
+          <p>2021</p>
+        </div>
+        <div className={styles.GithubContainer}>
+          <span>Visit source code on</span>
+          <a href="https://github.com/drull1000/website">
+            <FaGithub size="2rem" text-decoration="none" color="white" />
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
