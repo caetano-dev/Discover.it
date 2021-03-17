1ad136b47d2f22822346930b7307fda8bf85e2c4
Drull <pcaetanop@gmail.com>
2021-03-17 18:11:09 -0300

    Adding content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,107 @@
 import styles from "../styles/About-us.module.css";
-import { ImQuotesLeft, ImQuotesRight } from "react-icons/im";
+import { RiPlantLine } from "react-icons/ri";
 function Home() {
   return (
     <div className={styles.Home}>
       <h1>About</h1>
+      <h2>
+        We are an ecological traveling company <RiPlantLine />
+      </h2>
       <div>
-        <section>
-          <ImQuotesLeft size="2rem" />
-          <main>
+        <ul>
+          <li>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
               risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
               nec, ultricies sed, dolor.
             </p>
-          </main>
-          <ImQuotesRight size="2rem" />
-        </section>
+          </li>
+          <li>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+              risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
+              nec, ultricies sed, dolor.Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Sed non risus. Suspendisse lectus tortor,
+              dignissim sit amet, adipiscing nec, ultricies sed, dolor.
+            </p>
+          </li>
+          <li>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+              risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
+              nec, ultricies sed, dolor.Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Sed non risus. Suspendisse lectus tortor,
+              dignissim sit amet, adipiscing nec, ultricies sed, dolor.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+              Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
+              ultricies sed, dolor.
+            </p>
+          </li>
+          <li>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+              risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing t
+              amet, adipiscing
+            </p>
+          </li>
+          <li>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+              risus. Suspendisse lectus tortor, dignissim sit amet.
+            </p>
+          </li>
+          <li>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+              risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
+              nec, ultricies sed, dolor.Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Sed non risus. Suspendisse lectus tortor,
+              dignissim sit amet, adipiscing nec, ultricies sed, dolor.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+              Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
+              ultricies sed, dolor.
+            </p>
+          </li>
+        </ul>
+        <h1>Our purpose</h1>
+      <h2>
+        Explore the world <RiPlantLine />
+      </h2>
+      <ul>
+          <li>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+              risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
+              nec, ultricies sed, dolor.
+            </p>
+          </li>
+          <li>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+              risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
+              nec, ultricies sed, dolor.Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Sed non risus. Suspendisse lectus tortor,
+              dignissim sit amet, adipiscing nec, ultricies sed, dolor.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+              Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
+              ultricies sed, dolor.
+            </p>
+          </li>
+          <li>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+              risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
+              nec, ultricies sed, dolor.Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Sed non risus. Suspendisse lectus tortor,
+              dignissim sit amet, adipiscing nec, ultricies sed, dolor.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+              Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
+              ultricies sed, dolor.
+            </p>
+          </li>
+          </ul>
       </div>
-    </div>
+    </div>  
   );
 }
 
