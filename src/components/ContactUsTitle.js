ae61ebcffc0cdac7bb6796c2f346a73dcab58d3b
Drull <pcaetanop@gmail.com>
2021-03-18 17:43:03 -0300

    New components

---
+++ src/components/ContactUsTitle.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "../styles/components/ContactUsTitle.module.css";
+function ContactUsTitle() {
+  return (
+    <div className={styles.Contact}>
+      <h1>Get in contact</h1>
+    </div>
+  );
+}
+
+export default ContactUsTitle;
---
+++ src/components/Form.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "../styles/components/Form.module.css"
+export default function Form() {
+  return (
+    <div className={styles.Form}>
+      <div>
+        <form>
+            <div>
+          <label>
+            Name:
+            <input type="text"></input>
+          </label>
+          </div>
+          <div>
+          <label>
+            Country:
+            <input type="text"></input>
+          </label>
+          </div>
+          <div>
+          <label>
+            Phone number:
+            <input type="number"></input>
+          </label>
+          </div>
+          <div>
+          <label>
+            Trip date:
+            <input type="date"></input>
+          </label>
+          </div>
+          <div>
+          <button>Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
