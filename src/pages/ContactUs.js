3d007689d71c657e22166a881d36ddfbbb139120
Drull <pcaetanop@gmail.com>
2021-03-18 17:43:09 -0300

    New page

---
+++ src/pages/ContactUs.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ContactUsTitle from "../components/ContactUsTitle";
+import Form from "../components/Form";
+
+function ContactUs() {
+  return (
+    <div>
+      <ContactUsTitle />
+      <Form />
+    </div>
+  );
+}
+
+export default ContactUs;
