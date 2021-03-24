615127a90514f2f1de5cec036e997c197c3f9cff
Drull <pcaetanop@gmail.com>
2021-03-24 14:55:54 -0300

    Creating api

---
+++ src/Api.js
@@ -0,0 +1,47 @@
+import rainforest from "./assets/rainforest.jpeg";
+import rio from "./assets/rio.jpg";
+import brasilia from "./assets/brasilia.jpeg";
+import bonito from "./assets/bonito.jpeg";
+import dunes from "./assets/dunes.jpeg";
+import jericoacoara from "./assets/jericoacoara.jpeg";
+
+const Destinations = [
+  {
+    image: rainforest,
+    title: "Amazon Rainforest",
+    description: "Discover incredible landscapes.",
+    id: 1,
+  },
+  {
+    image: rio,
+    title: "Rio de Janeiro",
+    description: "Discover the city of samba.",
+    id: 2,
+  },
+  {
+    image: brasilia,
+    title: "Brasilia",
+    description: "Discover the capital of Brazil.",
+    id: 3,
+  },
+  {
+    image: bonito,
+    title: "Bonito",
+    description: "Discover incredible rivers.",
+    id: 4,
+  },
+  {
+    image: dunes,
+    title: "Lençóis Maranhenses",
+    description: "Discover the dunes.",
+    id: 5,
+  },
+  {
+    image: jericoacoara,
+    title: "Jericoacoara",
+    description: "Discover incredible beaches.",
+    id: 6,
+  },
+];
+
+export default Destinations;
