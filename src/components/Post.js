f68343e0217d5083fc8275136958016c7abbdd62
Drull <pcaetanop@gmail.com>
2021-03-24 14:56:01 -0300

    importing api

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,51 +1,6 @@
 import styles from "../styles/components/Post.module.css";
-import rainforest from "../assets/rainforest.jpeg";
-import rio from "../assets/rio.jpg";
-import brasilia from "../assets/brasilia.jpeg";
-import bonito from "../assets/bonito.jpeg";
-import dunes from "../assets/dunes.jpeg";
-import jericoacoara from "../assets/jericoacoara.jpeg";
-
+import Destinations from "../Api";
 function Post() {
-  const Destinations = [
-    {
-      image: rainforest,
-      title: "Amazon Rainforest",
-      description: "Discover incredible landscapes.",
-      id: 1,
-    },
-    {
-      image: rio,
-      title: "Rio de Janeiro",
-      description: "Discover the city of samba.",
-      id: 2,
-    },
-    {
-      image: brasilia,
-      title: "Brasilia",
-      description: "Discover the capital of Brazil.",
-      id: 3,
-    },
-    {
-      image: bonito,
-      title: "Bonito",
-      description: "Discover incredible rivers.",
-      id: 4,
-    },
-    {
-      image: dunes,
-      title: "Lençóis Maranhenses",
-      description: "Discover the dunes.",
-      id: 5,
-    },
-    {
-      image: jericoacoara,
-      title: "Jericoacoara",
-      description: "Discover incredible beaches.",
-      id: 6,
-    },
-  ];
-
   return (
     <>
       {Destinations.map((destinaton) => (
