99b12b210be2eba7ea882bfcefa48e28d1c63df5
Drull <pcaetanop@gmail.com>
2021-03-24 14:47:17 -0300

    Adding map function and array

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,64 @@
 import styles from "../styles/components/Post.module.css";
+import rainforest from "../assets/rainforest.jpeg";
+import rio from "../assets/rio.jpg";
+import brasilia from "../assets/brasilia.jpeg";
+import bonito from "../assets/bonito.jpeg";
+import dunes from "../assets/dunes.jpeg";
+import jericoacoara from "../assets/jericoacoara.jpeg";
 
-function Post({ image, title, description }) {
+function Post() {
+  const Destinations = [
+    {
+      image: rainforest,
+      title: "Amazon Rainforest",
+      description: "Discover incredible landscapes.",
+      id: 1,
+    },
+    {
+      image: rio,
+      title: "Rio de Janeiro",
+      description: "Discover the city of samba.",
+      id: 2,
+    },
+    {
+      image: brasilia,
+      title: "Brasilia",
+      description: "Discover the capital of Brazil.",
+      id: 3,
+    },
+    {
+      image: bonito,
+      title: "Bonito",
+      description: "Discover incredible rivers.",
+      id: 4,
+    },
+    {
+      image: dunes,
+      title: "Lençóis Maranhenses",
+      description: "Discover the dunes.",
+      id: 5,
+    },
+    {
+      image: jericoacoara,
+      title: "Jericoacoara",
+      description: "Discover incredible beaches.",
+      id: 6,
+    },
+  ];
+
   return (
-    <div className={styles.Container}>
-      <div className={styles.Post}>
-        <img src={image} alt={title} />
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <button>Learn more</button>
-      </div>
-    </div>
+    <>
+      {Destinations.map((destinaton) => (
+        <div key={destinaton.id} className={styles.Container}>
+          <div className={styles.Post}>
+            <img src={destinaton.image} alt={destinaton.title} />
+            <h2>{destinaton.title}</h2>
+            <p>{destinaton.description}</p>
+            <button>Learn more</button>
+          </div>
+        </div>
+      ))}
+    </>
   );
 }
 
