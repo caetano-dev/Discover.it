77620523f1b35238d824828031b3f9e2e8fef8ea
Drull <pcaetanop@gmail.com>
2021-03-26 17:02:38 -0300

    Deleting context

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,13 @@
 import Image from "../components/Image";
 import PostContainer from "../components/PostContainer";
 import styles from "../styles/Home.module.css";
-import { PostProvider } from "../contexts/PostContext";
 function Home() {
   return (
-    <PostProvider>
-      <div className={styles.Home}>
-        <Image />
-        <h1 className={styles.Text}>Destinations waiting for you:</h1>
-        <PostContainer />
-      </div>
-    </PostProvider>
+    <div className={styles.Home}>
+      <Image />
+      <h1 className={styles.Text}>Destinations waiting for you:</h1>
+      <PostContainer />
+    </div>
   );
 }
 
