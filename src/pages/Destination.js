09f3f34ed821a7b9b0ca45898a95a478b6b1a877
Drull <pcaetanop@gmail.com>
2021-03-26 16:18:22 -0300

    fetching data

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import styles from "../styles/components/Destination.module.css";
+import Destinations from "../Api";
+
 function Destination() {
+  const { id } = useParams();
   return (
     <div className={styles.Container}>
-      <div className={styles.Image}>
-        <h1>Destination name</h1>
-        <p>Destination description</p>
+      
+      <div className={styles.Title}>
+        <h1>{Destinations[id].title}</h1>
+        <p>{Destinations[id].description}</p>
+        <img src={Destinations[id].image} alt={Destinations[id].title} />
       </div>
       <div className={styles.Destination}>
         <p>
@@ -22,13 +28,7 @@ function Destination() {
           commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
           velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
           occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum.""Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est laborum."
+          mollit anim id est laborum."
         </p>
         <ul>
           <li>
