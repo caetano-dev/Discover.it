38c6edc34f6a4de1c37d691cbc823eff4c59bd0a
Drull <pcaetanop@gmail.com>
2021-03-28 17:04:42 -0300

    required

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,31 +8,31 @@ export default function Form() {
           <div>
             <label>
               Name:
-              <input type="text" required="true"></input>
+              <input type="text" required={true}></input>
             </label>
           </div>
           <div>
             <label>
               E-mail:
-              <input type="text" required="true"></input>
+              <input type="text" required={true}></input>
             </label>
           </div>
           <div>
             <label>
               Country:
-              <input type="text" required="true"></input>
+              <input type="text" required={true}></input>
             </label>
           </div>
           <div>
             <label>
               Phone number:
-              <input type="number" required="true"></input>
+              <input type="number" required={true}></input>
             </label>
           </div>
           <div>
             <label>
               Trip date:
-              <input type="date" required="true"></input>
+              <input type="date" required={true}></input>
             </label>
           </div>
           <div>
